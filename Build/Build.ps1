53eb007ab98456367236c088cab88dbb92a79d1b
Rick Strahl <rstrahl@west-wind.com>
2016-11-17 17:37:37 -1000

    Update Build Files

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -1,12 +1,15 @@
 cd "$PSScriptRoot" 
 
-"Copying files..."
-del .\Distribution\*.*
-copy ..\SaveImageToAzureBlob-MarkdownMonster-Addin\bin\Release\*.dll .\Distribution
+"Cleaning up build files..."
+del SaveImageToAzureBlob-MarkdownMonster-Addin.zip
 
+remove-item -recurse -force .\Distribution
+md Distribution
+
+
+"Copying files..."
+copy ..\SaveImageToAzureBlob-MarkdownMonster-Addin\bin\Release\*.dll .\Distribution
 del .\Distribution\NewtonSoft.Json.dll
 
 "Zipping up setup file..."
-7z a -tzip ".\SaveImageToAzureBlob-MarkdownMonster-Addin.zip" ".\Distribution\*.*" 
-
-pause
+7z a -tzip  SaveImageToAzureBlob-MarkdownMonster-Addin.zip .\Distribution\*.*
