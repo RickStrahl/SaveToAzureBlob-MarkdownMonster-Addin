14fb3daac84eb12ff6491c1bf8743d272e2aa9b7
Rick Strahl <rstrahl@west-wind.com>
2017-01-15 00:53:24 -1000

    Update build process to simplify pulling from output folder.

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -1,17 +1,18 @@
 cd "$PSScriptRoot" 
 
+
+$src = "$env:appdata\Markdown Monster\Addins\SaveImageToAzureBlobStorageAddin"
+
 "Cleaning up build files..."
 del addin.zip
 
 remove-item -recurse -force .\Distribution
 md Distribution
 
-
 "Copying files..."
-copy ..\SaveImageToAzureBlob-MarkdownMonster-Addin\bin\Release\*.dll .\Distribution
-copy version.json .\Distribution
-del .\Distribution\NewtonSoft.Json.dll
-
+copy $src\*.dll .\Distribution
+copy $src\version.json .\Distribution
+copy $src\version.json .\
 
 "Zipping up setup file..."
 7z a -tzip  addin.zip .\Distribution\*.*
