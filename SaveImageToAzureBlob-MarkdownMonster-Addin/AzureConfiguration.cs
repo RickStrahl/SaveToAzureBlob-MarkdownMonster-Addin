aba643e11685ca69de57ea00d8e9a6f96d581946
Rick Strahl <rstrahl@west-wind.com>
2017-04-14 00:00:34 -1000

    Refactor to use BaseConfiguration class to simplify configuration class.

--- SaveImageToAzureBlob-MarkdownMonster-Addin/AzureConfiguration.cs
+++ SaveImageToAzureBlob-MarkdownMonster-Addin/AzureConfiguration.cs
@@ -7,55 +7,54 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using MarkdownMonster;
+using MarkdownMonster.AddIns;
 using SaveImageToAzureBlobStorageAddin.Annotations;
 using Westwind.Utilities.Configuration;
 
 namespace SaveImageToAzureBlobStorageAddin
 {
-    public class AzureConfiguration : AppConfiguration, INotifyPropertyChanged  
-    {
-        public static AzureConfiguration Current;
-
+    public class AzureConfiguration : BaseAddinConfiguration<AzureConfiguration>
+    {        
         public List<AzureBlobConnection> ConnectionStrings { get; set; }
 
         static AzureConfiguration()
-        {
+        {            
             Current = new AzureConfiguration();
             Current.Initialize();
         }
         
         public AzureConfiguration()
         {
+            ConfigurationFilename = "SaveToAzureBlobStorageAddIn.json";
             ConnectionStrings = new List<AzureBlobConnection>();
         }
 
+                //#region AppConfiguration
+        //protected override IConfigurationProvider OnCreateDefaultProvider(string sectionName, object configData)
+        //{
+        //    var provider = new JsonFileConfigurationProvider<AzureConfiguration>()
+        //    {
+        //        JsonConfigurationFile = Path.Combine(mmApp.Configuration.CommonFolder, "SaveToAzureBlobStorageAddIn.json")
+        //    };
 
-        #region AppConfiguration
-        protected override IConfigurationProvider OnCreateDefaultProvider(string sectionName, object configData)
-        {
-            var provider = new JsonFileConfigurationProvider<AzureConfiguration>()
-            {
-                JsonConfigurationFile = Path.Combine(mmApp.Configuration.CommonFolder, "SaveToAzureBlobStorageAddIn.json")
-            };
+        //    if (!File.Exists(provider.JsonConfigurationFile))
+        //    {
+        //        if (!Directory.Exists(Path.GetDirectoryName(provider.JsonConfigurationFile)))
+        //            Directory.CreateDirectory(Path.GetDirectoryName(provider.JsonConfigurationFile));
+        //    }
 
-            if (!File.Exists(provider.JsonConfigurationFile))
-            {
-                if (!Directory.Exists(Path.GetDirectoryName(provider.JsonConfigurationFile)))
-                    Directory.CreateDirectory(Path.GetDirectoryName(provider.JsonConfigurationFile));
-            }
+        //    return provider;
+        //}
+        //#endregion
 
-            return provider;
-        }
-        #endregion
-
-        #region INotifyPropertyChanged
-        public event PropertyChangedEventHandler PropertyChanged;
+        //#region INotifyPropertyChanged
+        //public event PropertyChangedEventHandler PropertyChanged;
 
         
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
+        //protected virtual void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+        //#endregion
     }
 }
