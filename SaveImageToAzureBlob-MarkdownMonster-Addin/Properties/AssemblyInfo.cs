c641032f2dcb110902fd415bf59f5915fa6f55db
Rick Strahl <rstrahl@west-wind.com>
2018-06-06 21:31:48 -0700

    Update for latest Markdown Monster Version

--- SaveImageToAzureBlob-MarkdownMonster-Addin/Properties/AssemblyInfo.cs
+++ SaveImageToAzureBlob-MarkdownMonster-Addin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SaveImageToAzureBlobStorage")]
-[assembly: AssemblyDescription("Save Image To Azure Blob Storage Markdown Monster Addin")]
+[assembly: AssemblyTitle("Save Image To Azure Blob Storage Markdown Monster Addin")]
+[assembly: AssemblyDescription("Save images to Azure Blob storage")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("West Wind Technologies")]
 [assembly: AssemblyProduct("SaveToAzureBlobStorage")]
-[assembly: AssemblyCopyright("Copyright © West Wind Technologies 2016-2017")]
+[assembly: AssemblyCopyright("© Rick Strahl West Wind Technologies 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.15.0.0")]
+[assembly: AssemblyVersion("0.20.4.0")]
